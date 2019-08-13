ca6975c7706bcc5cc4a37a52a25da4bbbf57f3cb
Matt Westervelt <matthew.westervelt@intel.com>
2019-08-13 14:39:29 -0700

    [skip ci] add 3 shards, and mnist test

--- ci/buildkite.py
+++ ci/buildkite.py
@@ -83,26 +83,41 @@ def cmd_pipeline(args, remainder):
                     for shard in popt.get('run_shards'):
                         rsh = shard
                         shard = 'shard'
+                        tests.append(
+                            dict(suite=skey,
+                                 workload=wkey,
+                                 platform=pkey,
+                                 variant=variant,
+                                 timeout=popt.get('timeout', 20),
+                                 retry=popt.get('retry'),
+                                 softfail=popt.get('softfail'),
+                                 python=get_python(variant),
+                                 shard=shard,
+                                 shardcount=shc,
+                                 rsh=rsh,
+                                 emoji=get_emoji(variant),
+                                 engine=get_engine(pkey)))
+
                 else:
                     shard = None
 
                 for batch_size in suite['params'][args.pipeline]['batch_sizes']:
+                    if shc:
+                        continue
 
-                    tests.append(
-                        dict(suite=skey,
-                             workload=wkey,
-                             platform=pkey,
-                             batch_size=batch_size,
-                             variant=variant,
-                             timeout=popt.get('timeout', 20),
-                             retry=popt.get('retry'),
-                             softfail=popt.get('softfail'),
-                             python=get_python(variant),
-                             shard=shard,
-                             shardcount=shc,
-                             rsh=rsh,
-                             emoji=get_emoji(variant),
-                             engine=get_engine(pkey)))
+                    else:
+                        tests.append(
+                            dict(suite=skey,
+                                 workload=wkey,
+                                 platform=pkey,
+                                 batch_size=batch_size,
+                                 variant=variant,
+                                 timeout=popt.get('timeout', 20),
+                                 retry=popt.get('retry'),
+                                 softfail=popt.get('softfail'),
+                                 python=get_python(variant),
+                                 emoji=get_emoji(variant),
+                                 engine=get_engine(pkey)))
 
     if args.count:
         print('variants: {}'.format(len(variants)))
